187a8751d136a13f04c0e25baad3d0124ba6e4cb
bednarmartin <mar.bednar@outlook.com>
2023-07-02 14:31:13 +0200

    Initial commit

---
+++ src/main/java/com/bednarmartin/budgetmanagementsystem/BudgetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bednarmartin.budgetmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagementSystemApplication.class, args);
+	}
+
+}
